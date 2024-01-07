a2d7768f6910749954b9af62e63c242fc4a85e9a
+ <+>
2024-01-07 00:07:01 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Select the element you want to change the text of
+    var element = document.querySelector("#text");
+  
+   // sets the text content of the element to "This is really cool!"
+    element.textContent = "This is really cool!";
+});
